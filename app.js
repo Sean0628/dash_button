08fbfc0a45cf475be7cf69fbdc45e0c05cbd7985
Sho Ito <i.sho.0628@gmail.com>
2018-09-02 15:20:12 +0900

    add main logic to post a message to slack as you push the dash button

--- app.js
+++ app.js
@@ -1,13 +1,24 @@
+require('dotenv').config()
 const { WebClient } = require('@slack/client');
-const token = process.env.SLACK_TOKEN;
-const web = new WebClient(token);
-const conversationId = '';
-//warning this may trigger multiple times for one press
-//...usually triggers twice based on testing for each press
+const WEB = new WebClient(process.env.BOT_TOKEN);
+const TEXTS = [
+                'リゾートで飲み会発生中:beers:誰か一緒に飲もう:hibiscus::tropical_drink::palm_tree:',
+                'リゾートで飲み会発生中:beers:みんなあつまれ〜:man-running::dash::woman-running::dash:'
+              ]
+
+function getRandomInt(max) {
+  return Math.floor(Math.random() * Math.floor(max));
+}
+
+function pick_text(texts) {
+  return texts[getRandomInt(2)];
+}
+
 var dash_button = require('node-dash-button');
-var dash = dash_button("", null, null, 'all'); //address from step above
-dash.on("detected", function (){
-  web.chat.postMessage({ channel: conversationId, text: 'Hello, world' })
+var dash = dash_button(process.env.MAC_ADDRESS, null, null, 'all');
+
+dash.on('detected', function (){
+  WEB.chat.postMessage({ channel: process.env.CHANNEL_ID, text: pick_text(TEXTS) })
   .then((res) => {
     console.log('Message sent: ', res.ts);
   })
