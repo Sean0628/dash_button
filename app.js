97fc48c9af00f0e7a2aff8203e5d68a56acc7589
sean0628 <i.sho.0628@gmail.com>
2018-09-12 21:56:55 +0900

    mod messages

--- app.js
+++ app.js
@@ -2,8 +2,8 @@ require('dotenv').config()
 const { WebClient } = require('@slack/client');
 const WEB = new WebClient(process.env.BOT_TOKEN);
 const TEXTS = [
-                'リゾートで飲み会発生中:beers:誰か一緒に飲もう:hibiscus::tropical_drink::palm_tree:',
-                'リゾートで飲み会発生中:beers:みんなあつまれ〜:man-running::dash::woman-running::dash:'
+                '<!here|here>リゾートで飲み会発生中:beers:誰か一緒に飲もう:hibiscus::tropical_drink::palm_tree:',
+                '<!here|here>リゾートで飲み会発生中:beers:みんなあつまれ〜:man-running::dash::woman-running::dash:'
               ]
 
 function getRandomInt(max) {
@@ -15,7 +15,7 @@ function pick_text(texts) {
 }
 
 var dash_button = require('node-dash-button');
-var dash = dash_button(process.env.MAC_ADDRESS, null, null, 'all');
+var dash = dash_button(process.env.MAC_ADDRESS, null, 60000, 'all');
 
 dash.on('detected', function (){
   WEB.chat.postMessage({ channel: process.env.CHANNEL_ID, text: pick_text(TEXTS) })
